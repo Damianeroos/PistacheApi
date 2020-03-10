c4bf251a1b4b3168525523e2b56b5864462aad44
Damian Rakowski <damian.rakowski93@o2.pl>
2020-03-10 07:46:30 +0100

    proper signal processing

--- main.cpp
+++ main.cpp
@@ -1,39 +1,40 @@
 /*
-   Mathieu Stefani, 13 février 2016
+  Mathieu Stefani, 13 février 2016
 
-   Example of an hello world server
+  Example of an hello world server
 */
 #include "HelloHandler.hpp"
 #include <csignal>
 
 
-  Pistache::Http::Endpoint *endpoint_p;
+std::function<void(int)> shutdown_handler;
+void signal_handler(int signal) { shutdown_handler(signal); }
 
-void shutdown_handler(int no){
-  endpoint_p->shutdown();
-  std::cout << "\nShuting down server"<< std::endl;
-  exit(no);
-}
 int main() {
-  
-  std::signal(SIGABRT,SIG_IGN);
-  std::signal(SIGFPE,SIG_IGN);
-  std::signal(SIGILL,SIG_IGN);
-  std::signal(SIGSEGV,SIG_IGN);
-  std::signal(SIGTERM,SIG_IGN);
-  std::signal(SIGINT,shutdown_handler);
-  
-  Pistache::Address addr("localhost", Pistache::Port(1234));
-  auto router = std::make_shared<Pistache::Rest::Router>();    
-  auto opts = Pistache::Http::Endpoint::options().threads(1); 
+  try{
+    Pistache::Address addr("localhost", Pistache::Port(1234));
+    auto router = std::make_shared<Pistache::Rest::Router>();    
+    auto opts = Pistache::Http::Endpoint::options().threads(1);
+    Pistache::Http::Endpoint endpoint(addr);
+      
+    shutdown_handler = [&endpoint](int) { endpoint.shutdown();
+     std::cout << "\nShuting down server"<< std::endl;};
+    
+    for (auto sig : {SIGHUP, SIGINT, SIGQUIT, SIGTERM})
+      std::signal(sig, signal_handler);
  
-  Pistache::Http::Endpoint endpoint(addr);
-  endpoint_p = &endpoint;
-  endpoint.init(opts);
 
-  auto hello_handler = HelloHandler();
-  hello_handler.publish(router);
-  endpoint.setHandler(router->handler());
-  endpoint.serve();
+    endpoint.init(opts);
 
+    auto hello_handler = HelloHandler();
+    hello_handler.publish(router);
+    endpoint.setHandler(router->handler());
+    endpoint.serve();
+  }
+  catch(std::exception const& e){
+    std::cerr << "FATAL ERROR during program execution: " << e.what() << '\n';
+    return EXIT_FAILURE;
+  }
+  return EXIT_SUCCESS;
 }
+  
